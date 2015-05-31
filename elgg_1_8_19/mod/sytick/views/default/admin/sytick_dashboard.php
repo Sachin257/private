cc72788501f87c70f9fbc91b4db0b1d5c607152f
Sachin <sachinsunny257@gmail.com>
2015-05-31 13:14:25 +0530

    admin dashboard links and code optimization

--- elgg_1_8_19/mod/sytick/views/default/admin/sytick_dashboard.php
+++ elgg_1_8_19/mod/sytick/views/default/admin/sytick_dashboard.php
@@ -3,38 +3,35 @@
     <div class="adm_row nest_wrap">
         <div class="span4 adm_stats nest_wrap">
             <div class="adm_sites" style="width:100%">
-                <?php echo "SUMMARY";?>  <a href="/company/add/<?php echo CONSTRUCTION_TYPE_COMPANY?>" class="circle">x</a>
-                <span class="st_count"><a href="/company/all/<?php echo CONSTRUCTION_TYPE_COMPANY;?>"><?php echo $vars["constructing_active"];?></a></span>
+                <?php echo "SUMMARY";?>  <a href="#" class="circle">x</a>
+                <span class="st_count"><a href="#"></a></span>
             </div>
             <div class="adm_statbar column_right"></div>
             <div class="nest_wrap site_status status">
-                <div class="site_statcount column_left"><?php echo $vars["constructing_in_active"]?></div>
-                <div class="stat_text column_right"><?php //echo elgg_echo('admin:dashboard:pending_text')?> </div>
+                <div class="site_statcount column_left"></div>
+                <div class="stat_text column_right"></div>
             </div>
         </div>
         <div class="span4 nest_wrap">
             <div class="cnt_cmp" style="width:100%">
-                <?php echo "PROJECTS";?>  <a href="/company/add/<?php echo CONTRACTING_TYPE_COMPANY?>" class="circle">+</a>
-                <span class="st_count"><a href="/company/all/<?php echo CONTRACTING_TYPE_COMPANY;?>"><?php echo $vars["contracting_active"]?></a></span>
+                <?php echo "PROJECTS";?>  <a href="/painting_project/add/" class="circle">+</a>
+                <span class="st_count"><a href="/painting_project/all"><?=$vars["painting_projects"]; ?></a></span>
             </div>
             <div class="adm_statbar column_right"></div>
-            <div class="nest_wrap cntr_status status">
-                <div class="cntr_statcount column_left"><?php echo $vars["contracting_in_active"]?></div>
+            <div class="nest_wrap cntr_workstatus status">
+                <div class="cntr_workcount column_left"></div>
                 <div class="stat_text column_right"><?php //echo elgg_echo('admin:dashboard:pending_text')?></div>
             </div>
         </div>
         <div class="span4">
             <div class="cntr_work" style="width:100%">
-            	<?php 
-            	$profile_type_arr = get_custom_profile_types(CONTRACTOR_PROFILE_TYPE);
-            	?>
-                <?php echo "INVENTORY"?> <a href="/employee/add/<?php echo $profile_type_arr['guid']?>" class="circle">+</a>
-                <span class="st_count"><a href="/contractor/all"><?php echo $vars["active_contractor"];?></a></span>
+                <?php echo "INVENTORY"?> <a href="/paint/add/" class="circle">+</a>
+                <span class="st_count"><a href="/paint/all"><?= $vars["paints"];?></a></span>
             </div>
             <div class="adm_statbar column_right"></div>
             <div class="nest_wrap cntr_workstatus status">
-                <div class="cntr_workcount column_left"><?php echo $vars["in_active_contractor"];?></div>
-                <div class="stat_text column_right"><?php //echo elgg_echo('admin:dashboard:pending_text')?></div>
+                <div class="cntr_workcount column_left"></div>
+                <div class="stat_text column_right"></div>
             </div>
         </div>
     </div>
@@ -46,7 +43,7 @@
             <li><a href="#"><span class="ic_induction circle"></span>Alert<br />Settings</a></li>
             <li><a href="#"><span class="ic_evemanager circle"></span>Calculations</a></li>
             <li><a href="#"><span class="ic_mgmaccount circle"></span>Efficency<br />Settings</a></li>
-            <li><a href="#"><span class="ic_useraccount circle"></span><?php echo elgg_echo("entity:Securities"); ?></a></li>
+            <li><a href="#"><span class="ic_useraccount circle"></span><?php //echo elgg_echo("entity:Securities"); ?></a></li>
             <li><span class="ic_reports circle"></span>Reports</li>
         </ul>
     </div>
