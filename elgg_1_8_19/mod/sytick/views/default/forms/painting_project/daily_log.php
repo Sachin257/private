dce4fa2d2bc6bc74e741a2b9589d4854b8c0ad1d
Sachin <sachinsunny257@gmail.com>
2015-05-31 16:11:54 +0530

    added new fields in add daily work modal

--- elgg_1_8_19/mod/sytick/views/default/forms/painting_project/daily_log.php
+++ elgg_1_8_19/mod/sytick/views/default/forms/painting_project/daily_log.php
@@ -1,4 +1,4 @@
-<?php //print "<pre>";print_r($vars['contracting_companies']) ;print "</pre>"; //exit;  ?>
+<?php //print "<pre>";print_r($vars['contracting_companies']) ;print "</pre>"; //exit;   ?>
 <div class="modal fade" id="daily_log_add" style="display: none;" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -23,24 +23,32 @@
                         <span class="error_msg clear paint_err"></span>
                         <div class="clear"></div>
                         <div class="userrow">
-                            
-        <label><?php echo elgg_echo("project:nopieces"); ?></label>
-        <?php echo elgg_view('input/text', array('name' => 'nopieces', "id" => "nopieces")); ?>
-        <span class="error_msg clear nopieces_err"></span>      
+
+                            <label><?php echo elgg_echo("project:nopieces"); ?></label>
+                            <?php echo elgg_view('input/text', array('name' => 'nopieces', "id" => "nopieces")); ?>
+                            <span class="error_msg clear nopieces_err"></span>      
                         </div>
                         <div class="userrow">
-                        
-                        
-                    </div>
-                </div>      
-            </div>
-            <div align="center" class="MT_two">
-                <?php
-                echo elgg_view('input/hidden', array('name' => 'project_guid', 'id' => 'project_guid', "value" => $vars["entity"]->guid));
-                ?>	
-                <?php echo elgg_view('input/button', array('id' => 'btn_daily_save', "class" => "btn btn-default btn_submit_grey", 'value' => elgg_echo('certificate:add'))); ?>
-                <?php echo elgg_view('input/button', array('id' => 'btn_quest_cancel', "data-dismiss" => "modal", "class" => "btn btn-default btn_submit_grey", 'value' => elgg_echo('certificate:cancel'))); ?><span class="ajx-submit-load disp-none"></span>
+                            <label><?php echo elgg_echo("paint:used:shift"); ?></label>
+                            <?php   $options = array("Moring" => "Moring" , "Noon" => "Noon" );
+                                    $value = array("Moring" => "Moring");
+                                    echo elgg_view('input/radio', array('name' => 'shift', "id" => "shift" , "options" => $options, "value" => $value)); ?>
+                            <span class="error_msg clear shift_err"></span>
+                        </div>
+                        <div class="userrow">
+                            <label><?php echo elgg_echo("paint:user:employee"); ?></label>
+                            <?php echo elgg_view('input/text', array('name' => 'employee_name', "id" => "employee_name")); ?>
+                            <span class="error_msg employee_name_err"></span>
+                        </div>
+                    </div>      
+                </div>
+                <div align="center" class="MT_two">
+                    <?php
+                    echo elgg_view('input/hidden', array('name' => 'project_guid', 'id' => 'project_guid', "value" => $vars["entity"]->guid));
+                    ?>	
+                    <?php echo elgg_view('input/button', array('id' => 'btn_daily_save', "class" => "btn btn-default btn_submit_grey", 'value' => elgg_echo('certificate:add'))); ?>
+                    <?php echo elgg_view('input/button', array('id' => 'btn_daily_cancel', "data-dismiss" => "modal", "class" => "btn btn-default btn_submit_grey", 'value' => elgg_echo('certificate:cancel'))); ?><span class="ajx-submit-load disp-none"></span>
+                </div>
             </div>
         </div>
     </div>
-</div>
