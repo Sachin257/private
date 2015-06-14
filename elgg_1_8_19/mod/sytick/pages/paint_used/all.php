9616bf9d129147900471ce3af5dccd28b615b5c2
Sachin <sachinsunny257@gmail.com>
2015-06-14 09:49:24 +0530

    paint_used listing page

--- elgg_1_8_19/mod/sytick/pages/paint_used/all.php
+++ elgg_1_8_19/mod/sytick/pages/paint_used/all.php
@@ -1,4 +1,87 @@
-<?php 
-/**
- * Needs to be done
- */
+<?php
+gatekeeper();
+elgg_register_title_button();
+
+
+$limit = get_input('limit', SYTICK_PAGINATION_LIMIT);
+$offset = get_input('offset', 0);
+$s = get_sanitised_input('s');
+
+if($segments[1])
+{
+  $offset =  $segments[1];
+}
+
+$add_link = elgg_view('output/url', array(
+		'href' => "paint_used/add",
+		'text' => elgg_echo('paint_used:add'),
+		'class' => 'elgg-button elgg-button-add float-alt'
+));
+$body  = '<div class="title-button-container">' . $add_link . '</div>';
+$title = elgg_echo('paint_used:head');
+$body_title = elgg_view_title($title,array('class'=>"companytitle"));
+$search_arr = array(
+		'types' => 'object',
+		'subtypes' => 'paint_used',
+		'limit' => ELGG_ENTITIES_NO_VALUE
+);
+
+$paint_used_entities = elgg_get_entities_from_metadata($search_arr);
+
+if(count($paint_used_entities)){
+	foreach($paint_used_entities as $_paint_entities){
+		$paint_used_entities_ids[] = $_paint_entities->guid;
+	}
+}
+
+if(count($paint_used_entities_ids)){
+	if($s != "")
+	{
+		$paint_used_entities_ids = get_paint_search($s, join($paint_used_entities_ids,",") );
+	}
+	if(count($paint_used_entities_ids)){
+		$options = array(
+				'type' => 'object',
+				'subtype'=>'paint_used',
+				'guid' => $paint_used_entities_ids,
+				'limit' => $limit,
+				'offset' => $offset,
+				'count' => TRUE,
+		);
+
+		$count = elgg_get_entities_from_metadata($options);
+		
+		$options = array(
+				'type' => 'object',
+				'subtype'=>'paint_used',
+				'guid' => $paint_used_entities_ids,
+				'limit' => $limit,
+				'offset' => $offset,
+		);
+		$paint_used_entities = elgg_get_entities_from_metadata($options);
+		
+		$pagination = elgg_view('navigation/pagination',array(
+				'base_url' => '/paint_used/all',
+				'offset' => $offset,
+				'count' => $count,
+				'limit' => $limit,
+		));
+	} else {
+		$paint_used_entities = false;
+		$pagination = "";
+	
+	}
+} else {
+	$paint_used_entities = false;
+	$pagination = "";
+
+}
+
+
+$body .= elgg_view('paint_used/list',array("paint"=>$paint_used_entities, "offset"=>$offset));
+
+$body .= $pagination;
+
+$body = elgg_view_layout('one_column', array('title' => $body_title,'content' => $body));
+
+echo elgg_view_page($title, $body);
--- elgg_1_8_19/mod/sytick/views/default/paint_used/list.php
+++ elgg_1_8_19/mod/sytick/views/default/paint_used/list.php
@@ -5,66 +5,34 @@ $offset = elgg_extract('offset', $vars);
 <div class="clear"></div>
 <div class="cmp_list rounded_corners frm_box_shadow frm_box_border MB_five mgr_listall">
     <div class="search_box nest_wrap "> 
-    <h3 class="column_left"><?php echo elgg_echo('enitiy:paint')?></h3>
-    <div class="column_right"><?php echo  elgg_view_form("paint/filter", array('id'=>'frm_paint_search'), $vars);?></div>
+        <h3 class="column_left" style="color: #3a5795;"><?php echo elgg_echo('paint_used:head') ?></h3>
     </div>
     <div>
-    <?php if($paint_entities && count($paint_entities)) { ?>
-    <table class="elgg-list elgg-list-entity mgr_list">	
-		<tbody>
-		<tr>
-                        <th style="width: 20%"><?php echo elgg_echo('entity:name')?></th>
-			<th style="width: 20%"><?php echo elgg_echo('paint:id')?></th>
-			<th style="width: 15%"><?php echo elgg_echo('paint:colour')?></th>
-                        <th style="width: 15%"><?php echo elgg_echo('paint:price')?></th>
-                        <th style="width: 20%"><?php echo elgg_echo('paint:current:quantity')?></th>
-			<th style="width: 10%"></th>
-		</tr>
-		<?php foreach($paint_entities as $paint_entitie)  {?>
-		<tr id="elgg-object-<?php echo $paint_entitie->guid?>" class="elgg-item">
-			<td class="company-title">
-			<?php  echo elgg_view('output/url', array(
-					'href' => "#",
-					'text' => $paint_entitie->title,
-					'class' => '',
-					'rel' => 'nofollow'
-			)); ?>
-			</td>
-                        <td><?php
-                                $edit_url = "/paint/edit/$paint_entitie->guid";
-                                echo elgg_view('output/url', array(
-					'href' => $edit_url,
-					'text' => $paint_entitie->paint_id,
-					'class' => '',
-					'rel' => 'nofollow'
-                                ));?></td>
-                        <td><?php echo $paint_entitie->colour;?></td>
-                        <td style="text-align: left;"><?php echo $paint_entitie->price;?></td>
-                        <td><?php echo $paint_entitie->quantity;?></td>
-			<td class="company-link">
-			<?php						
-			echo elgg_view('output/url', array(
-					'href' => $edit_url,
-					'text' => elgg_echo('entity:edit'),
-					'class' => 'MR_20 elgg-button-edit-information float-alt',
-					'rel' => 'nofollow'
-			));
-			$delete_url = "/action/paint/delete/?guid={$paint_entitie->guid}&offset=$offset";
-			echo elgg_view('output/confirmlink', array(
-					'href' => $delete_url,
-					'text' => elgg_echo('entity:delete'),
-					'class' => 'elgg-button-delete float-alt'
-			));
-			?>
-			</td>
-		</tr>
-		<?php } ?>
-		</tbody>
-	</table>
-	<?php } else { ?>
-	<?php echo elgg_echo('paint:none')?>	
-	<?php }?>
-</div>
+        <?php if ($paint_entities && count($paint_entities)) { ?>
+            <table class="elgg-list elgg-list-entity mgr_list">	
+                <tbody>
+                    <tr>
+                        <th><?php echo elgg_echo("entity:date")?></th>
+                        <th><?php echo elgg_echo('entity:name') ?></th>
+                        <th><?php echo elgg_echo('paint:id') ?></th>
+                        <th><?php echo elgg_echo('paint:colour') ?></th>
+                        <th><?php echo elgg_echo('paint:used:quantity') ?></th>
+                    </tr>
+                    <?php foreach ($paint_entities as $paint_entitie) { ?>
+                        <tr id="elgg-object-<?php echo $paint_entitie->guid ?>" class="elgg-item" style="height: 40px;">
+                            <td><?php echo date("d-m-Y",$paint_entitie->time_created); ?></td>
+                            <td><?php echo $paint_entitie->title; ?></td>
+                            <td><?php echo $paint_entitie->paint_used_id; ?></td>
+                            <td><?php echo $paint_entitie->colour; ?></td>
+                            <td><?php echo $paint_entitie->quantity; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } else { ?>
+            <?php echo elgg_echo('paint:none') ?>	
+        <?php } ?>
+    </div>
 
-<div class="clear">
-</div>
+    <div class="clear">
+    </div>
